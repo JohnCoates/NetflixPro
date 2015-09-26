edd2196c12b1f15858b497a4b575670809c55e2a
John Coates <acapulco1988@gmail.com>
2015-09-26 15:44:47 -0700

    removed commented code, logging, clarified variable name

--- src/embedded/embedded.js
+++ src/embedded/embedded.js
@@ -1,20 +1,15 @@
 function NFPRewind(seconds) {
   var videoPlayer = window.netflix.cadmium.objects.videoPlayer();
   var currentTimeMS = videoPlayer.getCurrentTime();
-  var secondsInMS = seconds * 1000;
-  var seekBackToTime = currentTimeMS - secondsInMS;
-  // var roundedSeekBackTenSeconds = Math.floor(seekBackTenSeconds / 1000) * 1000;
-  //
-  // if (roundedSeekBackTenSeconds < 0) {
-  //   roundedSeekBackTenSeconds = 0;
-  // }
+  var rewindMS = seconds * 1000;
+  var seekBackToTime = currentTimeMS - rewindMS;
   videoPlayer.seek(seekBackToTime);
 }
 
 $NFP(document).on("click", ".icon-player-rewind10", function(event) {
-      console.log("rewinding!");
       NFPRewind(10);
 });
+
 $NFP(document).on("click", ".icon-player-rewind30", function(event) {
       NFPRewind(30);
 });
