aa2af1864916ab00388a2b14b23e4d95e963aa3b
John Coates <acapulco1988@gmail.com>
2015-09-26 02:57:20 -0700

    removed timed function, clicks now attach to document

--- src/embedded/embedded.js
+++ src/embedded/embedded.js
@@ -1,16 +1,3 @@
-var seekTo = setInterval(function() {
-  clearInterval(seekTo);
-  console.log("seeking!");
-  // var videoPlayer = window.netflix.cadmium.objects.videoPlayer();
-  // var currentTimeMS = videoPlayer.getCurrentTime();
-  // var tenSecondsMS = 10 * 1000;
-  // var seekBackTenSeconds = currentTimeMS - tenSecondsMS;
-  // var roundedSeekBackTenSeconds = Math.floor(seekBackTenSeconds/1000)*1000;
-  // videoPlayer.seek(roundedSeekBackTenSeconds);
-  // console.log("getCurrentTime: " + videoPlayer.getCurrentTime());
-
-}, 2000);
-
 function NFPRewind(seconds) {
   var videoPlayer = window.netflix.cadmium.objects.videoPlayer();
   var currentTimeMS = videoPlayer.getCurrentTime();
@@ -24,9 +11,10 @@ function NFPRewind(seconds) {
   videoPlayer.seek(seekBackToTime);
 }
 
-$NFP(".icon-player-rewind10").on("click", function(event) {
+$NFP(document).on("click", ".icon-player-rewind10", function(event) {
+      console.log("rewinding!");
       NFPRewind(10);
 });
-$NFP(".icon-player-rewind30").on("click", function(event) {
+$NFP(document).on("click", ".icon-player-rewind30", function(event) {
       NFPRewind(30);
 });
